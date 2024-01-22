df4c0191e46cb7aae4332cce58e5ccdc891ff0d3
Canu Fabien <fabien.canu@gmail.com>
2024-01-22 14:04:57 +0100

    Up

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,24 +3,24 @@ import { useEffect, useState } from "react";
 import Option from "./Option";
 
 function Form() {
-  const [listTasksByUser, setListTasksByUser] = useState([])
-  const [currentUser, setCurrentUser] = useState(1)
-  const [error, setError] = useState(null)
+  const [listTasksByUser, setListTasksByUser] = useState([]);
+  const [currentUser, setCurrentUser] = useState("1d");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(`https://dummyjson.com/todos/user/${currentUser}`)
-      .then(res => res.json())
-      .then(res => {
-        if(!res.message) {
-          setError(null)
-          setListTasksByUser(res.todos)
-        }
-        else setError(res.message);
+      .then((res) => res.json())
+      .then((res) => {
+        if (!res.message) {
+          setError(null);
+          setListTasksByUser(res.todos);
+        } else setError(res);
       });
-    }, [currentUser])
+  }, [currentUser]);
 
   return (
     <form>
+      {error && <div className="notice">{error.message}</div>}
       <label htmlFor="task">Ajouter une tâche</label>
       <div className="fields">
         <input type="text" id="task" placeholder="Intitulé de la tâche" />
@@ -32,11 +32,6 @@ function Form() {
         </select>
       </div>
       <button type="submit">Ajouter</button>
-
-      <hr />
-      {error && <>{error}</>}
-      <input type="text" value={currentUser} onChange={(e)=> {setCurrentUser(e.target.value)}} /> 
-
     </form>
   );
 }
